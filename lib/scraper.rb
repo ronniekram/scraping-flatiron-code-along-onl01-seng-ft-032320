a26596393b571066f25fe79e13319187d8a25bf5
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-07 00:30:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,8 @@ class Scraper
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
   
-  def get_courses 
+  def get_courses
+    self.get_page.css(".post")
   end 
   
   def print_courses
